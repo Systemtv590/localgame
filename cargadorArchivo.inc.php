2095490e2c790f406799d550c1ad8712f41edafd
Systemtv590 <81577070+Systemtv590@users.noreply.github.com>
2021-03-29 18:47:14 -0400

    Add files via upload

---
+++ cargadorArchivo.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+$fuentesJavaScript = array(
+    "js/ajax.js",
+	"js/teclado.js",
+	"js/buclePrimario.js",
+	"js/rectangulo.js",
+	"js/mando.js",
+	"js/inicio.js"
+);
+
+foreach($fuentesJavaScript as $fuente) {
+	echo  'esto es una prueba';
+	echo nl2br("\n");
+}
